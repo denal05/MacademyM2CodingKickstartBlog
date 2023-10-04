fce7fd8c317e62f9cccb07556f24fba9fd79ca23
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-10-04 15:21:42 +0200

    Added routes.xml and a controller; also refactored the vendor_module parent directories. 	new file:   Controller/Index/Index.php 	new file:   etc/frontend/routes.xml renamed:     from: denal05/m-academy-m2-coding-kickstart-blog     to:   Denal05/MacademyM2CodingKickstartBlog

---
+++ Controller/Index/Index.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Denal05\MacademyM2CodingKickstartBlog\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class Index implements HttpGetActionInterface
+{
+    public function execute()
+    {
+        die('Hello world');
+    }
+}
