f44438ea24808e0721f0a89bc8ec03fb6be1aed5
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-10-05 09:36:10 +0200

    Used a Magento\Framework\View\Result\PageFactory to return a Page in /blog/post/detail/ 	modified:   Controller/Post/Detail.php

--- Controller/Post/Detail.php
+++ Controller/Post/Detail.php
@@ -3,11 +3,22 @@
 namespace Denal05\MacademyM2CodingKickstartBlog\Controller\Post;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
 
 class Detail implements HttpGetActionInterface
 {
-    public function execute()
+    public function __construct(
+        private PageFactory $pageFactory,
+        private RequestInterface $request,
+    ) {}
+
+    public function execute(): Page
     {
-        die('/blog/post/detail');
+//        echo "<pre>";
+//        var_dump($this->request->getParams());
+//        echo "</pre>";
+        return $this->pageFactory->create();
     }
 }
