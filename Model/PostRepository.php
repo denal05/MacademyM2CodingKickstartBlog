c1d7912ca32cccfeb522cb53abb3184be5671d12
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-10-07 12:33:39 +0200

    Implemented getById($id), save($post), and deleteById($id) in Model\PostRepository 	modified:   Model/PostRepository.php

--- Model/PostRepository.php
+++ Model/PostRepository.php
@@ -4,22 +4,59 @@ namespace Denal05\MacademyM2CodingKickstartBlog\Model;
 
 use Denal05\MacademyM2CodingKickstartBlog\Api\Data\PostInterface;
 use Denal05\MacademyM2CodingKickstartBlog\Api\PostRepositoryInterface;
+use Denal05\MacademyM2CodingKickstartBlog\Model\PostFactory;
+use Denal05\MacademyM2CodingKickstartBlog\Model\ResourceModel\Post as PostResourceModel;
+use Magento\Framework\Exception\CouldNotDeleteException;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class PostRepository implements PostRepositoryInterface
 {
 
+    public function __construct(
+        private PostFactory $postFactory, // Since we're in the Model dir, we don't have to import Model\Post
+        private PostResourceModel $postResourceModel,
+    ) {}
+
     public function getById(int $id): PostInterface
     {
-        // TODO: Implement getById() method.
+        $post = $this->postFactory->create();       // Fetch a Blank Post object.
+        $this->postResourceModel->load($post, $id); // Load the blank Post object with DB data.
+
+        if (!$post->getId()) {
+            throw new NoSuchEntityException(
+                __('The blog post with id %1 doesn\'t exist.', $id)
+            );
+        }
+
+        return $post;
     }
 
     public function save(PostInterface $post): PostInterface
     {
-        // TODO: Implement save() method.
+        try {
+            $this->postResourceModel->save($post);
+        } catch (\Exception $e) {
+            throw new CouldNotSaveException(
+                __($e->getMessage())
+            );
+        }
+
+        return $post;
     }
 
     public function deleteById(int $id): bool
     {
-        // TODO: Implement deleteById() method.
+        $post = $this->getById($id);
+
+        try {
+            $this->postResourceModel->delete($post);
+        } catch (\Exception $e) {
+            throw new CouldNotDeleteException(
+                __($e->getMessage())
+            )
+        }
+
+        return true;
     }
 }
