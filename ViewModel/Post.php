1e340607ab8d93d8e1004b8d3fac166f1a8277ea
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-10-07 20:35:01 +0200

    Loaded data in ViewModel\Post using the Api\PostRepositoryInterface, included ViewModel\Post as a block argument to blog_post_detail.xml, and loaded real DB data at the URL /blog/post/detail/id/1 	modified:   ViewModel/Post.php 	modified:   view/frontend/layout/blog_post_detail.xml 	modified:   view/frontend/templates/post/detail.phtml 	modified:   view/frontend/templates/post/list.phtml 	modified:   view/frontend/templates/post/sidebar.phtml

--- ViewModel/Post.php
+++ ViewModel/Post.php
@@ -2,13 +2,19 @@
 
 namespace Denal05\MacademyM2CodingKickstartBlog\ViewModel;
 
+use Denal05\MacademyM2CodingKickstartBlog\Api\Data\PostInterface;
+use Denal05\MacademyM2CodingKickstartBlog\Api\PostRepositoryInterface;
 use Denal05\MacademyM2CodingKickstartBlog\Model\ResourceModel\Post\Collection;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class Post implements ArgumentInterface
 {
     public function __construct(
         private Collection $collection,
+        private PostRepositoryInterface $postRepository,
+        private RequestInterface $request,
     ) {}
 
     public function getList(): array
@@ -19,5 +25,14 @@ class Post implements ArgumentInterface
     public function getCount(): int
     {
         return $this->collection->count();
+    }
+
+    /**
+     * @throws LocalizedException
+     */
+    public function getDetail(): PostInterface
+    {
+        $id = (int) $this->request->getParam('id');
+        return $this->postRepository->getById($id);
     }
 }
