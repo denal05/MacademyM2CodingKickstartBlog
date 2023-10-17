3dc38745c194ba9a483594e528b35b2d63599802
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-10-17 13:07:56 +0200

    Added a confirmation dialog to an admin grid delete action. 	modified:   Ui/Component/Listing/Column/Actions.php

--- Ui/Component/Listing/Column/Actions.php
+++ Ui/Component/Listing/Column/Actions.php
@@ -2,6 +2,7 @@
 
 namespace Denal05\MacademyM2CodingKickstartBlog\Ui\Component\Listing\Column;
 
+use Magento\Framework\Escaper;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponentFactory;
@@ -12,8 +13,12 @@ class Actions extends Column
     /** @var UrlInterface */
     protected $urlBuilder;
 
+    /** @var Escaper */
+    protected $escaper;
+
     /**
      * Actions constructor.
+     * @param Escaper $escaper
      * @param ContextInterface $context
      * @param UiComponentFactory $uiComponentFactory
      * @param UrlInterface $urlBuilder
@@ -21,12 +26,14 @@ class Actions extends Column
      * @param array $data
      */
     public function __construct(
+        Escaper $escaper,
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
         UrlInterface $urlBuilder,
         array $components = [],
         array $data = []
     ) {
+        $this->escaper = $escaper;
         $this->urlBuilder = $urlBuilder;
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
@@ -42,6 +49,8 @@ class Actions extends Column
                 continue;
             }
 
+            $title = $this->escaper->escapeHtml($item['title']);
+
             $item[$this->getData('name')] = [
                 'edit' => [
                     'href' => $this->urlBuilder->getUrl('macademym2codingkickstartblog/post/edit', [
@@ -54,6 +63,11 @@ class Actions extends Column
                         'id' => $item['id'],
                     ]),
                     'label' => __('Delete'),
+                    'confirm' => [
+                        'title' => __('Delete "%1"', $title),
+                        'message' => __('Are you sure you want to delete: "%1"', $title),
+                    ],
+                    'post' => true,
                 ],
             ];
         }
