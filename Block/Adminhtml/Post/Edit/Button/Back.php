f0c8f0b62684c325371d1794a805cd14da28526c
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-10-25 14:09:31 +0200

    Created a Back button to cancel an admin form submission. 	new file:   Block/Adminhtml/Post/Edit/Button/Back.php 	modified:   view/adminhtml/ui_component/denal05_macademym2codingkickstartblog_post_form.xml

---
+++ Block/Adminhtml/Post/Edit/Button/Back.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Denal05\MacademyM2CodingKickstartBlog\Block\Adminhtml\Post\Edit\Button;
+
+use http\Url;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+class Back implements ButtonProviderInterface
+{
+    /** @var UrlInterface $urlBuilder */
+    private UrlInterface $urlBuilder;
+
+    /**
+     * @param UrlInterface $url
+     */
+    public function __construct(
+        UrlInterface $url
+    ) {
+        $this->urlBuilder = $url;
+    }
+
+    /**
+     * @return array
+     */
+    public function getButtonData(): array
+    {
+        $url = $this->urlBuilder->getUrl('*/*/');
+
+        return [
+            'label' => __('Back'),
+            'class' => 'back',
+            'on_click' => sprintf("location.href = '%s';", $url)
+        ];
+    }
+}
